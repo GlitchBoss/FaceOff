3ee527550473c7c4a0991625958487b380744151
GlitchBoss <theprograminator@gmail.com>
2016-03-23 17:22:19 -0400

    Added Tie Breaker

---
+++ Assets/GLITCH/Scripts/Utilities/SceneUtility.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace GLITCH.Utilities
+{
+	public class SceneUtility : MonoBehaviour
+	{
+		private static List<GameObject> dontDestroy = new List<GameObject>();
+		public static string currentScene = "MainMenu";
+
+		public static void LoadScene(Scene scene)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				UnityEngine.Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(scene.name);
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, scene);
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public static void LoadScene(int build)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				UnityEngine.Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(build);
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneAt(build));
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public static void LoadScene(string name)
+		{
+			foreach (GameObject go in dontDestroy.ToArray())
+			{
+				UnityEngine.Object.DontDestroyOnLoad(go);
+			}
+			SceneManager.LoadScene(name);
+			currentScene = name;
+			foreach (GameObject go in dontDestroy)
+			{
+				//SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneByName(name));
+				go.SendMessage("StartUp");
+			}
+		}
+
+		public void MoveAllObjects(string sceneName)
+		{
+			foreach (GameObject go in dontDestroy)
+			{
+				SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneByName(sceneName));
+				StartCoroutine(Delay(go));
+			}
+		}
+
+		IEnumerator Delay(GameObject go)
+		{
+			yield return new WaitForSeconds(0.1f);
+			go.SendMessage("StartUp", SendMessageOptions.DontRequireReceiver);
+		}
+
+		public static void DontDestroyOnLoad(GameObject go)
+		{
+			dontDestroy.Add(go);
+		}
+	}
+}
---
+++ Assets/Scripts/System/Deactivator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deactivator : MonoBehaviour {
+
+	public GameObject[] deactivate;
+	public float delay;
+
+	public void OnStart()
+	{
+		StartCoroutine(Deactivate(delay));
+	}
+
+	IEnumerator Deactivate(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		foreach(GameObject go in deactivate)
+		{
+			go.SetActive(false);
+		}
+	}
+}
